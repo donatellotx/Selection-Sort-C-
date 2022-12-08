16c6ee7044507bc3d5b009732267091a75c1915b
Donte Mayfield <donatello409@hotmail.com>
2022-12-08 16:45:16 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Selection_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stuff = new int[] { 8, 6, 7, 5, 3, 0, 9 };
+            var stuffCount = stuff.Length;
+            int temp;
+            int smalls;
+
+            for(int p = 0; p < stuffCount; p++)
+            {
+                smalls = p;
+                for(int i = p + 1; i < stuffCount; i++)
+                {
+                    var currentComparisonValue = stuff[i];
+                    var currentItemValue = stuff[smalls];
+                    if (currentComparisonValue < currentItemValue)
+                    {
+                        smalls = i;
+                    }
+                }
+                if (smalls != p)
+                {
+                    temp = stuff[smalls];
+                    stuff[smalls] = stuff[p];
+                    stuff[p] = temp;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
